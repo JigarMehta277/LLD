09831535876bec0887af4188635bd5fd38288f22
JigarMehta277 <jigarmehta277@gmail.com>
2025-04-21 13:09:36 -0400

    Added main.py with Coffee and Ingredient class, also updated readme file with project requirenments

---
+++ Coffee Vending Machine/main.py
@@ -0,0 +1,31 @@
+class Coffee:
+    def __init__(self, name, price, recipe):
+        self.name = name
+        self.price = price
+        self.recipe = recipe
+
+    def get_name(self):
+        return self.name
+    
+    def get_price(self):
+        return self.price
+    
+    def get_recipe(self):
+        return self.recipe
+    
+class Ingredient:
+    def __init__(self, name, quantity):
+        self.name = name
+        self.quantity = quantity
+
+    def get_name(self):
+        return self.name
+    
+    def get_quantity(self):
+        return self.quantity
+    
+    def update_quantity(self, amount):
+        self.quantity += amount
+
+    
+        
