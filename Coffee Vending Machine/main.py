e3cb6950d51a213aa152a07bc378d33caef78814
JigarMehta277 <jigarmehta277@gmail.com>
2025-04-21 13:14:11 -0400

    Added payment class

--- Coffee Vending Machine/main.py
+++ Coffee Vending Machine/main.py
@@ -27,5 +27,15 @@ class Ingredient:
     def update_quantity(self, amount):
         self.quantity += amount
 
+class Payment:
+    def __init__(self, amount):
+        self.amount = amount
+
+    def get_amount(self):
+        return self.amount
+    
+    
+
+
     
         
