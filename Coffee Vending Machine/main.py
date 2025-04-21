ccb6dd704d1626bdaefc139c83be5550d5bbc23e
JigarMehta277 <jigarmehta277@gmail.com>
2025-04-21 13:32:10 -0400

    Added coffeeMachine class and created staticmethods

--- Coffee Vending Machine/main.py
+++ Coffee Vending Machine/main.py
@@ -1,7 +1,31 @@
 from Coffee import coffee
 from Ingredient import ingredient
 
+class coffeeMachine:
+    _instance = None
 
+    def __init__(self):
+        if coffeeMachine._instance is not None:
+            raise Exception("This class is a singleton!")
+        else:
+            coffeeMachine._instance = self
+            self.coffee_menu = []
+            self.ingredients = {}
+            self._initialize_ingredients()
+            self._initialize_coffee_menu()
+
+    @staticmethod
+    def get_instance():
+        if coffeeMachine._instance is None:
+            coffeeMachine()
+        return coffeeMachine._instance
+    
+    def _initialize_coffee_menu(self):
+        espresso_recipe = {
+            self.ingredients["Coffee"]: 1,
+            self.ingredients["Water"]: 1
+        }
+        self.coffee_menu.append(coffee("Espresso", 2.5, espresso_recipe))
     
 
 
