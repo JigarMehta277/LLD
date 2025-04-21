f1b1b4bfdee2f478696adbc3468e456310450d8c
JigarMehta277 <jigarmehta277@gmail.com>
2025-04-21 13:18:18 -0400

    Seperating classes to their own files

--- Coffee Vending Machine/main.py
+++ Coffee Vending Machine/main.py
@@ -1,40 +1,6 @@
-class Coffee:
-    def __init__(self, name, price, recipe):
-        self.name = name
-        self.price = price
-        self.recipe = recipe
 
-    def get_name(self):
-        return self.name
     
-    def get_price(self):
-        return self.price
-    
-    def get_recipe(self):
-        return self.recipe
-    
-class Ingredient:
-    def __init__(self, name, quantity):
-        self.name = name
-        self.quantity = quantity
 
-    def get_name(self):
-        return self.name
-    
-    def get_quantity(self):
-        return self.quantity
-    
-    def update_quantity(self, amount):
-        self.quantity += amount
-
-class Payment:
-    def __init__(self, amount):
-        self.amount = amount
-
-    def get_amount(self):
-        return self.amount
-    
-    
 
 
     
